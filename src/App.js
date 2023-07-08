038317ed7f1b03d9f338d317859de659009f5e8e
Guy Kadosh <guyk50@gmail.com>
2023-07-08 11:53:27 +0300

    Created fake API with json.server and fetching it, initial state with useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import DateCounter from './DateCounter'
+import Main from './Main'
+import Header from './Header'
+import { useEffect, useReducer } from 'react'
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      }
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      }
+    default:
+      throw new Error('Action unkonwn')
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(function () {
+    fetch('http://localhost:9000/questions')
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(err => dispatch({ type: 'dataFailed' }))
+  }, [])
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </div>
   )
 }
